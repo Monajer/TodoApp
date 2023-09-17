54084ae7a1c9beffff35f3805221cc6730452abc
Monajer <monajeralhakim@gmail.com>
2023-09-17 20:06:07 +0600

    commit from vscode

--- maine.js
+++ maine.js
@@ -3,6 +3,8 @@ let textInput = document.getElementById("textInput")
 let dateInput = document.getElementById("dateInput")
 let textarea = document.getElementById("textarea")
 let msg = document.getElementById("msg")
+let tasks = document.getElementById("tasks")
+let add = document.getElementById("add")
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault()
@@ -17,6 +19,7 @@ let formValidation = ()=>{
         console.log("success")
         msg.innerHTML = ""
         acceptData()
+        add.setAttribute("data-bs-dismiss", "modal")
     }
 }
 
@@ -24,8 +27,28 @@ let data = {}
 
 let acceptData = ()=>{
     data ["text"] = textInput.value
-    // data ["date"] = dateInput.value
     data ["date"] = dateInput.value
-    data ["Description"] = textarea.value
-    console.log(data)
+    data ["description"] = textarea.value
+    createTasks()
+}
+
+let createTasks = ()=>{
+    tasks.innerHTML += 
+    `<div>
+        <span class="fw-bold">${data.text}</span>
+        <span class="small text-secondary">${data.date}</span>
+        <p>${data.description}</p>
+
+        <span class="options">
+        <i class="fa-solid fa-pen-to-square"></i>
+        <i class="fa-solid fa-trash"></i>                    
+    </span>
+    </div>`
+    resetForm()
+}
+
+let resetForm = ()=>{
+    textInput.value = ""
+    dateInput.value = ""
+    textarea.value = ""
 }
