a93a572a9bcae73184205e95f38b7f9b8cf84686
Monajer <monajeralhakim@gmail.com>
2023-09-19 15:16:43 +0600

    last

--- maine.js
+++ maine.js
@@ -11,20 +11,17 @@ form.addEventListener("submit", (e)=>{
     formValidation()
 })
 
-let formValidation = ()=>{
-    if(textInput.value === ""){
-        console.log("failure")
-        msg.innerHTML = "Task cannot be blank"
-    } else {
-        console.log("success")
-        msg.innerHTML = ""
-        acceptData()
-        add.setAttribute("data-bs-dismiss", "modal")
-        add.click()
-
-        (()=>{
-            add.setAttribute("data-bs-dismiss", "")
-        })()
+let formValidation = () => {
+  if (textInput.value === "") {
+    console.log("failure");
+    msg.innerHTML = "Task cannot be blank";
+  } else {
+    console.log("success");
+    msg.innerHTML = "";
+    acceptData();
+    add.setAttribute("data-bs-dismiss", "modal");
+    add.click();
+    add.setAttribute("data-bs-dismiss", "");
     }
 }
 
@@ -32,47 +29,47 @@ let data = []
 
 let acceptData = ()=>{
     data.push({
-        text: textInput.value,
-        date: dateInput.value,
-        description: textarea.value,
+        text : textInput.value,
+        date : dateInput.value,
+        description : textarea.value,
     })
 
-    localStorage.setItem("data", data)
-    // localStorage.getItem
+    localStorage.setItem("data", JSON.stringify(data))
 
     console.log(data)
-
     createTasks()
-
 }
 
 let createTasks = ()=>{
-    tasks.innerHTML += 
-    `<div>
-        <span class="fw-bold">${data.text}</span>
-        <span class="small text-secondary">${data.date}</span>
-        <p>${data.description}</p>
-
-        <span class="options">
-        <i onClick ="editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fa-solid fa-pen-to-square"></i>
-        <i onClick ="deleteTask(this)" class="fa-solid fa-trash"></i>                    
-    </span>
-    </div>`
+    tasks.innerHTML = ""
+    data.map((x, y)=>{
+        return (tasks.innerHTML += `
+        <div id=${y}>
+            <span class="fw-bold">${x.text}</span>
+            <span class="small text-secondary">${x.date}</span>
+            <p>${x.description}</p>
+    
+            <span class="options">
+             <i onClick ="editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fa-solid fa-pen-to-square"></i>
+             <i onClick ="deleteTask(this)" class="fa-solid fa-trash"></i>                    
+            </span>
+        </div>
+        `)
+    })
 
     resetForm()
-
 }
 
-let deleteTask = (e)=>{
-    e.parentElement.parentElement.remove()
+let deleteTask = (e) => {
+    e.parentElement.parentElement.remove();
 }
 
-let editTask = (d)=>{
-    let selectedTask = d.parentElement.parentElement
+let editTask = (e) => {
+    let selectedTask = e.parentElement.parentElement;
 
-    textInput.value = selectedTask.children[0].innerHTML
-    dateInput.value = selectedTask.children[1].innerHTML
-    textarea.value = selectedTask.children[2].innerHTML
+    textInput.value = selectedTask.children[0].innerHTML;
+    dateInput.value = selectedTask.children[1].innerHTML;
+    textarea.value = selectedTask.children[2].innerHTML;
 
     selectedTask.remove()
 }
@@ -82,3 +79,10 @@ let resetForm = ()=>{
     dateInput.value = ""
     textarea.value = ""
 }
+
+(()=>{
+    data = JSON.parse(localStorage.getItem("data"))
+    createTasks()
+    console.log(data)
+
+})()
